e946837719d592164a66607674c95f6d686ea105
ssewilliam <deriwilliams2008@gmail.com>
2018-06-30 12:30:12 +0300

    fix database url

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    DATABASE_URL = "postgresql://postgres:password@localhost:5432/trackerdb"
+    DATABASE_URL = "postgres://ehbhssmcsvljgt:61e3f44a97a3d0ee9c05ce13d06fe6692fd3186394137a1e2c9d6aae4c3b96c9@ec2-54-235-196-250.compute-1.amazonaws.com:5432/d45koq7e85dqii"
 
 
 class TestingConfig(Config):
